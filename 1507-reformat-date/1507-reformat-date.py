44e8826f55545044e48bbfd84f2a3a2a75b8540d
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-02 05:50:39 +0530

    Time: 28 ms (89.36%), Space: 13.9 MB (97.90%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -24,4 +24,12 @@ class Solution:
         # if len(date) == 1:
         #     date = "0"+date
         # return f"{year}-{month}-{date}"
-        return D.strptime(re.sub('rd|nd|th|st|','',date),'%d %b %Y').strftime('%Y-%m-%d')
+        # return D.strptime(re.sub('rd|nd|th|st|','',date),'%d %b %Y').strftime('%Y-%m-%d')
+        M = {"Jan" : "01", "Feb" : "02", "Mar" : "03", "Apr" : "04", "May" : "05", "Jun" : "06", "Jul" : "07", "Aug" : "08", "Sep" : "09", "Oct" : "10", "Nov" : "11", "Dec" : "12", }
+        
+        D = ""
+        if (len(date) == 13):
+            D += date[-4:] + "-" + M[date[-8:-5]] + "-" + date[:2]
+        else:
+            D += date[-4:] + "-" + M[date[-8:-5]] + "-0" + date[0]
+        return D
