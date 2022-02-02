d10974f3e5263b32507a612906cefa88d42b3039
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-02 08:00:43 +0530

    Time: 56 ms (44.11%), Space: 15.2 MB (24.12%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.py
+++ 412-fizz-buzz/412-fizz-buzz.py
@@ -1,13 +1,27 @@
 class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
+        valueDefinition = {
+            3: "Fizz",
+            5: "Buzz"
+        }
         answer = []
         for i in range(1,n+1):
-            if i % 3 == 0 and i % 5 == 0:
-                answer.append("FizzBuzz")
-            elif i % 3 == 0:
-                answer.append("Fizz")
-            elif i % 5 == 0:
-                answer.append("Buzz")
-            else:
-                answer.append(str(i))
+            currentString = ""
+            for k,v in valueDefinition.items():
+                if i % k == 0:
+                    currentString+=v
+            if currentString == "":
+                currentString = str(i)
+            answer.append(currentString)
         return answer
+        # answer = []
+        # for i in range(1,n+1):
+        #     if i % 3 == 0 and i % 5 == 0:
+        #         answer.append("FizzBuzz")
+        #     elif i % 3 == 0:
+        #         answer.append("Fizz")
+        #     elif i % 5 == 0:
+        #         answer.append("Buzz")
+        #     else:
+        #         answer.append(str(i))
+        # return answer
