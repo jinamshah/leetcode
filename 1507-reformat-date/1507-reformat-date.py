872fac14800acb4e69e72a34642f6a4e4c3be7ff
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-02 05:49:28 +0530

    Time: 40 ms (45.85%), Space: 14.1 MB (70.35%) - LeetHub

--- 1507-reformat-date/1507-reformat-date.py
+++ 1507-reformat-date/1507-reformat-date.py
@@ -1,25 +1,27 @@
 import re
+from datetime import datetime as D
 class Solution:
     def reformatDate(self, date: str) -> str:
-        values = date.split(" ")
-        months = {
-            "Jan": "01", 
-            "Feb": "02",
-            "Mar": "03",
-            "Apr": "04",
-            "May": "05",
-            "Jun": "06",
-            "Jul": "07",
-            "Aug": "08",
-            "Sep": "09",
-            "Oct": "10",
-            "Nov": "11",
-            "Dec": "12"
-        }
-        year = values[2]
-        month = months[values[1]]
-        # print(values)
-        date = re.sub(r"th|rd|nd|st", "", values[0])
-        if len(date) == 1:
-            date = "0"+date
-        return f"{year}-{month}-{date}"
+        # values = date.split(" ")
+        # months = {
+        #     "Jan": "01", 
+        #     "Feb": "02",
+        #     "Mar": "03",
+        #     "Apr": "04",
+        #     "May": "05",
+        #     "Jun": "06",
+        #     "Jul": "07",
+        #     "Aug": "08",
+        #     "Sep": "09",
+        #     "Oct": "10",
+        #     "Nov": "11",
+        #     "Dec": "12"
+        # }
+        # year = values[2]
+        # month = months[values[1]]
+        # # print(values)
+        # date = re.sub(r"th|rd|nd|st", "", values[0])
+        # if len(date) == 1:
+        #     date = "0"+date
+        # return f"{year}-{month}-{date}"
+        return D.strptime(re.sub('rd|nd|th|st|','',date),'%d %b %Y').strftime('%Y-%m-%d')
