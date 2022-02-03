9c88dd3fc02b24432add0726c609c9ca73b90045
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-03 08:12:41 +0530

    Time: 75 ms (30.95%), Space: 13.8 MB (98.26%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,20 +1,28 @@
+import re
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_vals = {
-            'I': 1,
-            'V': 5,
-            'X': 10,
-            'L': 50,
-            'C': 100,
-            'D': 500,
-            'M': 1000
+        romanDict = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
         }
-        s = list(s)
-        total = 0
-        for i in range(len(s) - 1):
-            if roman_vals[s[i]] < roman_vals[s[i+1]]:
-                total -= roman_vals[s[i]]
-            else:
-                total += roman_vals[s[i]]
-        total += roman_vals[s[-1]]
-        return total
+        edgeCases = {
+            "IV": "IIII",
+            "IX": "VIIII",
+            "XL": "XXXX",
+            "XC":"LXXXX",
+            "CD": "CCCC",
+            "CM": "DCCCC"
+        }
+        answer = 0
+        for key,val in edgeCases.items():
+            s = s.replace(key,val)
+        for char in s:
+            answer += romanDict[char]
+        return answer
+            
+            
