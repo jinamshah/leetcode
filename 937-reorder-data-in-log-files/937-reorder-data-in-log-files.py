5cfdfe983ac20a3610eed33e6fb80100853fb634
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-07 10:04:44 +0530

    Time: 40 ms (69.36%), Space: 14 MB (96.34%) - LeetHub

--- 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
+++ 937-reorder-data-in-log-files/937-reorder-data-in-log-files.py
@@ -9,8 +9,6 @@ class Solution:
                 digit_logs.append(log)
             else:
                 letter_logs.append([identifier,log_data])
-        print(letter_logs)
-        # letter_logs = dict(sorted(letter_logs.items(), key = lambda kv:kv[1], kv[0]))
         letter_logs = sorted(letter_logs, key = lambda x: (x[1],x[0]))
         letter_logs = [k+" "+v for k,v in letter_logs]
         return letter_logs + digit_logs
