baea71c678fb8155d6f06f90644f171be05ce583
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-02-24 00:17:58 +0530

    Time: 117 ms (44.17%), Space: 15.2 MB (58.18%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,6 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
         for i in range(len(nums)):
-            remainder = target-nums[i]
-            if remainder in nums and nums.index(remainder) != i:
-                return [i, nums.index(target-nums[i])]
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            remainder = target - nums[i]
+            if remainder in hashmap and hashmap[remainder] != i:
+                return [i,hashmap[remainder]]
