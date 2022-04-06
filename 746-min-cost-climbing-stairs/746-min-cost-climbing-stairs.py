fe31bf006f7282d0dd21f18ec8ac5cbe279ea8b5
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-04-06 14:34:35 -0400

    Time: 85 ms (47.49%), Space: 16.2 MB (12.76%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.py
@@ -1,11 +1,12 @@
 class Solution:
-    def helper(i, cost, minCost):
-        if minCost[i] > 0:
-            return minCost[i]
-        minCost[i] = min((minCost[i-1] + cost[i-1]), (minCost[i-2] + cost[i-2]))
-        return minCost[i]
+    
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        minCost = [0] * (len(cost)+1)
-        for i in range(2, len(cost)+1):
-            minCost[i] = min((minCost[i-1] + cost[i-1]), (minCost[i-2]+cost[i-2]))
-        return minCost[len(cost)]
+        def helper(i):
+            if i <=1:
+                return 0
+            elif i in cache:
+                return cache[i]
+            cache[i] = min((helper(i-1) + cost[i-1]), (helper(i-2)+cost[i-2]))
+            return cache[i]
+        cache = {}
+        return helper(len(cost))
