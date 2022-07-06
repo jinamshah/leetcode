2061d708185f4ac7894ea8e6b2facd0767ef631f
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-07-06 12:00:49 -0500

    Time: 66 ms (32.70%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -1,9 +1,9 @@
 class Solution:
     def fib(self, n: int) -> int:
-        if n < 2:
+        first_num = 0
+        second_num = 1
+        if n == 0 or n == 1:
             return n
-        cache = [0] * (n+1)
-        cache[1] = 1
-        for i in range(2, n+1):
-            cache[i] = cache[i-1] + cache[i-2]
-        return cache[n]
+        for i in range (2,n+1):
+            first_num, second_num = second_num, (first_num+second_num)
+        return second_num
