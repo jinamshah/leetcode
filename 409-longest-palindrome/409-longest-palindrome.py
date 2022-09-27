1d26433ffd68069735b5c960df244c158818b6c5
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-09-27 13:53:55 -0400

    Time: 69 ms (18.47%), Space: 14 MB (22.60%) - LeetHub

--- 409-longest-palindrome/409-longest-palindrome.py
+++ 409-longest-palindrome/409-longest-palindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         answer = 0
         hashtable = collections.Counter(s)
+        hashtable = {k:v for k,v in sorted(hashtable.items(), key=lambda x: x[1], reverse=True)}
         for char,val in hashtable.items():
             answer += val//2 * 2
             if answer % 2 == 0 and val % 2 == 1:
