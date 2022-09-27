2682c45f5877a6213ce4a7c556b63963e57c3e42
Jinam Shah <40169508+jinamshah@users.noreply.github.com>
2022-09-27 12:30:25 -0400

    Time: 116 ms (17.67%), Space: 17.3 MB (94.34%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -5,12 +5,23 @@
 #         self.next = None
 
 class Solution:
+    def getIntersection(self, head):
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return slow
     def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head or not head.next:
+        if not head:
             return None
-        arr = [head]
-        while arr[-1].next:
-            arr.append(arr[-1].next)
-            if arr[-1].next in arr:
-                return arr[-1].next
-        return None
+        intersection = self.getIntersection(head)
+        
+        if not intersection:
+            return None
+        ptr1, ptr2 = head, intersection
+        while ptr1 != ptr2:
+            ptr1 = ptr1.next
+            ptr2 = ptr2.next
+            
+        return ptr1
