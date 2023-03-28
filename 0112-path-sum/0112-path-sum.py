93e3b8b69157484c6cf217b6572f9e9764c6bdc4
Jinam Shah <jinams47@gmail.com>
2023-03-28 12:20:10 -0400

    Time: 51 ms (20.20%), Space: 14.9 MB (88.44%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -8,7 +8,17 @@ class Solution:
     def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
         if not root:
             return False
-        if targetSum == root.val and not root.left and not root.right:
-            return True
-        return self.hasPathSum(root.left, targetSum-root.val) or self.hasPathSum(root.right, targetSum-root.val)
+        queue = collections.deque([(root,targetSum)])
+        while queue:
+            current = queue.popleft()
+            currentNode = current[0]
+            currentTarget = current[1]
+            
+            if currentTarget - currentNode.val == 0 and not currentNode.left and not currentNode.right:
+                return True
+            if currentNode.right:
+                queue.appendleft((currentNode.right, currentTarget-currentNode.val))
+            if currentNode.left:
+                queue.appendleft((currentNode.left, currentTarget-currentNode.val))
+            
         return False
