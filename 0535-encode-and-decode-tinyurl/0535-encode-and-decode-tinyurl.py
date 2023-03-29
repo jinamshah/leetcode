9c559e65d65076a6abbb8e4e84be3211aef655ba
Jinam Shah <jinams47@gmail.com>
2023-03-28 21:01:37 -0400

    Time: 45 ms (7.24%), Space: 14.5 MB (6.68%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.py
@@ -1,22 +1,20 @@
 import uuid
 class Codec:
     def __init__(self):
-        self.hashset=set()
+        self.hashset={}
     def encode(self, longUrl: str) -> str:
         """Encodes a URL to a shortened URL.
         """
-        encodedUrls = [x[0] for x in self.hashset]
-        if longUrl not in encodedUrls:
-            self.hashset.add((longUrl,str(uuid.uuid4())))
-        return [x[1] for x in self.hashset if x[0] == longUrl][0]
+        # encodedUrls = [x[0] for x in self.hashset]
+        if longUrl not in self.hashset.values():
+            encoded = uuid.uuid4()
+            self.hashset[encoded] = longUrl
+        return encoded
 
     def decode(self, shortUrl: str) -> str:
         """Decodes a shortened URL to its original URL.
         """
-        print(self.hashset)
-        print("asdas: ",shortUrl)
-        decodedUrls = [x[0] for x in self.hashset if x[1] == shortUrl]
-        return decodedUrls[0]
+        return self.hashset[shortUrl]
 
 # Your Codec object will be instantiated and called as such:
 # codec = Codec()
